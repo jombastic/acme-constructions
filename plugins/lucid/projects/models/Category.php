13be316a85d9d639354ebd2204f313d61e1d06f0
Slavcho Mitrov <slavcho_mitrov@yahoo.com>
2023-11-01 16:15:10 +0100

    create projects plugin

---
+++ plugins/lucid/projects/models/Category.php
@@ -0,0 +1,33 @@
+<?php namespace Lucid\Projects\Models;
+
+use Model;
+
+/**
+ * Model
+ */
+class Category extends Model
+{
+    use \Winter\Storm\Database\Traits\Validation;
+
+
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'lucid_projects_categories';
+
+    /**
+     * @var array Validation rules
+     */
+    public $rules = [
+    ];
+
+    // Attach images
+    public $attachOne = [
+        'thumbnail' => 'System\Models\File',
+        'featured_image' => 'System\Models\File'
+    ];
+
+    public $hasMany = [
+        'projects' => 'Lucid\Projects\Models\Project'
+    ];
+}
